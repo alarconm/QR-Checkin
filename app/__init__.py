fd55f33db5ede166c24fbc1ad614accec7eed79a
Michael <malarconii@yahoo.com>
2022-12-05 08:11:40 -0800

    updated views and routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request, redirect
-from app.views import scan, account
 import os
+import openpyxl
+from io import BytesIO
+from PIL import Image
+from time import sleep
+import cv2
+from pyzxing import BarCodeReader
 
 app = Flask(__name__)
 
@@ -12,10 +17,69 @@ os.environ.setdefault('FLASK_APP', 'app')
 def home():
     return render_template('home.html')
 
-@app.route('/scan')
+
+@app.route('/scan', methods=['GET', 'POST'])
 def scan():
-    # Code to handle a user scanning a QR code here
-    return render_template('scan.html')
+    # Initialize the QR code reader
+    reader = cv2.QRCodeDetector()
+
+    # Capture frames from the user's camera
+    cam = cv2.VideoCapture(0)
+
+    # Flag to track whether a QR code has been decoded
+    qr_decoded = False
+    while not qr_decoded:
+        # Read a frame from the camera
+        _, frame = cam.read()
+
+        # Decode any QR codes in the frame
+        data, bbox, _ = reader.detectAndDecode(frame)
+        if data:
+            # Set the flag to true
+            qr_decoded = True
+
+        # Display the frame to the user
+        cv2.imshow('Frame', frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    # Release the camera and destroy the window displaying the video feed
+    cam.release()
+    cv2.destroyAllWindows()
+
+    if not data:
+        return render_template('scan.html', error='Failed to decode QR code')
+
+    # At this point, the variable 'data' should contain the data from the QR code.
+
+    # Extract the first and last names from the QR code data
+    # Assume the data is encoded as "first_name last_name"
+    try:
+        first_name, last_name = qr_data.split(' ')
+    except:
+        return render_template('scan.html', error='QR code data is not in the expected format')
+
+
+    # Open the Excel file and search for the customer's name on the roster
+    excel_file = openpyxl.load_workbook('roster.xlsx')
+    sheet = excel_file.get_sheet_by_name('Sheet1')  # Assume the roster is on the first sheet
+    found = False
+    for row in sheet.rows:
+        if row[0].value == first_name and row[1].value == last_name:
+            # Assume the first and last names are in the first and second columns
+            found = True
+            break
+
+    if found:
+        # Customer's name was found on the roster
+        return render_template('scan.html', found=True)
+    else:
+        # Customer's name was not found on the roster
+        return render_template('scan.html', found=False)
+
+
+
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -34,9 +98,3 @@ def login():
             return render_template('login.html', error_msg=error_msg)
     else:
         return render_template('login.html')
-
-
-
-#@app.route('/account')
-#def account():
-# Code for the account view function
